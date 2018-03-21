205533fc3414d0a603b3cdf6662ac058b9801e6b
Mark De May <markd@frii.com>
2018-03-21 10:09:11 -0600

    Model tests.

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "missing todo field" do
+    # todo = Todo.new({completed: false})
+    todo = Todo.new({})
+    assert !todo.valid?, "Todo should not be valid without a todo"
+  end
+
+  test "presence of todo field" do
+    todo = Todo.new({todo: "stuff", completed: false})
+    assert todo.valid?, "Todo should be valid with a todo"
+  end
+
+  test "should not save without todo" do
+    todo = Todo.new
+    assert_not todo.save, "Saved the todo without a todo field"
+  end
 end
