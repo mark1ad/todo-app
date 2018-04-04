d7ee375830c1d5911c2c0230a89e2f341df83629
Mark De May <markd@frii.com>
2018-04-04 09:44:26 -0600

    Upgraded to rails 5.1.5 Added end-to-end rspec tests. Added view test for home.

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.0.6'
+gem 'rails', '~> 5.1.5'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.18'
 # Use Puma as the app server
@@ -44,6 +44,7 @@ gem 'bootstrap-sass'
 
 group :development, :test do
   gem 'rspec-rails', '~> 3.6'
+  gem "capybara"
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  it "missing todo field" do
+  it "is not valid without a todo" do
     todo = Todo.new
     todo.valid?
     expect(todo.errors[:todo]).to include("can't be blank")
   end
 
-  it "presence of todo field" do
+  it "is valid with a todo" do
     todo = Todo.new({todo: "stuff"})
     todo.valid?
     expect(todo.errors[:todo].count).to equal(0)
   end
 
-  it "default completed value" do
+  it "has default completed value" do
     todo = Todo.new
     expect(todo.completed).to equal(false)
   end
