6d073ead3f664d06d508258a5d16166a0332da18
Mark De May <markd@frii.com>
2018-04-19 09:25:43 -0600

    Add todo end-to-end tests

--- spec/system/add_todo_spec.rb
+++ spec/system/add_todo_spec.rb
@@ -1,22 +1,70 @@
 require "rails_helper"
 
-RSpec.describe "adding a todo", type: :system do
-  it "allows a user to create a todo" do
+RSpec.describe "End-to-end tests", type: :system do
+  describe "adding a todo" do
+    it "allows a user to create a todo" do
+      visit root_path
+      fill_in "todo[todo]", with: "Todo 1"
+      click_on("Add")
+      visit root_path
+      @todo = Todo.find_by(todo: "Todo 1")
+      expect(page).to have_selector("#todo_#{@todo.id} .todo-name", text: "Todo 1")
+    end
+
+    it "does not allow a user to create a todo without text" do
+      visit root_path
+      fill_in "todo[todo]", with: ""
+      click_on("Add")
+      visit root_path
+      # There should be no todos on page therefore no object with a
+      # class="todo-name"
+      expect(page).not_to have_selector(".todo-name")
+    end
+  end
+
+  xit "allows a user to delete a todo" do
+    pending "not implemented yet"
+    # Create the todo
+    visit root_path
+    fill_in "todo[todo]", with: "Todo 1"
+    click_on("Add")
+    visit root_path
+
+    # Click the delete link
+    @todo = Todo.find_by(todo: "Todo 1")
+    # TODO: Getting
+    # Capybara::NotSupportedByDriverError:
+    #   Capybara::Driver::Base#accept_modal
+    # Can't find fix
+    accept_confirm do
+      click_link("todo-delete-#{@todo.id}")
+    end
+  end
+
+  xit "allows a user to complete a todo" do
+    # Create the todo
+    visit root_path
+    fill_in "todo[todo]", with: "Todo 1"
+    click_on("Add")
+    visit root_path
+
+    # complete todo
+    # TODO: Can't get form to submit
+    @todo = Todo.find_by(todo: "Todo 1")
+    @completed_checkbox = find("#todo_completed_#{@todo.id}")
+    check("todo_completed_#{@todo.id}")
+    execute_script("$('form#todo_form_#{@todo.id}').submit()")
+    visit root_path
+    expect(page.find("#todo_completed_#{@todo.id}")).to be_checked
+  end
+
+  it "todo is not complete" do
+    # Create the todo
     visit root_path
     fill_in "todo[todo]", with: "Todo 1"
     click_on("Add")
     visit root_path
     @todo = Todo.find_by(todo: "Todo 1")
-    expect(page).to have_selector("#todo_#{@todo.id} .todo-name", text: "Todo 1")
-  end
-
-  it "does not allow a user to create a todo without text" do
-    visit root_path
-    fill_in "todo[todo]", with: ""
-    click_on("Add")
-    visit root_path
-    # There should be no todos on page therefore no object with a
-    # class="todo-name"
-    expect(page).not_to have_selector(".todo-name")
+    expect(page.find("#todo_completed_#{@todo.id}")).not_to be_checked
   end
 end
